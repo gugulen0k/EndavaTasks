7aed45c44d4648c0858043f8eef4670f3b7558e5
gugulen0k <terzi.kirill.ceiti@gmail.com>
2021-11-24 20:26:55 +0200

    Made desktop version

--- LandingPage/DesignHomework/main.js
+++ LandingPage/DesignHomework/main.js
@@ -2,3 +2,24 @@ import "./style.css";
 import { init } from "./scripts/sphere";
 
 init();
+
+const items = document.querySelector(".features ul");
+items.addEventListener("mouseover", (e) => {
+  if (e.target.nodeName === "LI" || e.target.nodeName === "IMG") {
+    document.querySelector(".description .text").textContent =
+      e.target.dataset.text;
+  }
+});
+
+window.onscroll = function () {
+  let button = document.querySelector("#sticky-button button");
+  let sticky = button.offsetTop;
+  console.log(sticky);
+
+  if (window.pageYOffset > sticky) {
+    button.classList.add("sticky");
+  }
+  if (window.pageYOffset < 500) {
+    button.classList.remove("sticky");
+  }
+};
